b3b1764a9b694ce6648d75b5cdd755e885389304
Krynox <deridderjames@hotmail.com>
2015-01-10 13:02:27 +0100

    test3

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));.
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images',express.static(path.join(__dirname, 'public/images')));
 app.use('/src',express.static(path.join(__dirname, 'public/src')));
 app.use('/src/scripts',express.static(path.join(__dirname, 'public/src/scripts/')));
